1deeeb815188c8d21fff4fbfdd3a2febc1717f6c
Setsuko Bergsjo <setea.music@gmail.com>
2025-12-06 17:08:02 +0900

    Add customer and repository interface

---
+++ CManager.Presentation.consoleApp/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,10 @@
+﻿using CManager.Presentation.consoleApp.Models;
+
+namespace CManager.Presentation.consoleApp.Interfaces;
+
+public interface ICustomerRepository
+{
+    void SaveAllCustomers(List<Customer> customers);
+
+    List<Customer> GetAllCustomers();
+}
