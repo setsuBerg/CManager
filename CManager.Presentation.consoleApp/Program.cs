48cd59942f8fa67e924458691c3d56e388d430ee
Setsuko Bergsjo <setea.music@gmail.com>
2025-12-18 15:17:54 +0100

    Fix Program.cs and cleanup class1

--- CManager.Presentation.consoleApp/Program.cs
+++ CManager.Presentation.consoleApp/Program.cs
@@ -3,8 +3,8 @@ using CManager.Application.Interfaces;
 using CManager.Application.Repositories;
 using CManager.Application.Services;
 
-var repository = new CustomerRepository();
-var service = new CustomerService(repository);
+ICustomerRepository repository = new CustomerRepository();
+ICustomerService service = new CustomerService(repository);
 var menu = new MenuController(service);
 
 menu.Start();
