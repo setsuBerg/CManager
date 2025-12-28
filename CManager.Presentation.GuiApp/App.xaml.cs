eb81376e3ded500894904dce9a7c1c2abe7c0098
Setsuko Bergsjo <setea.music@gmail.com>
2025-12-28 19:19:12 +0100

    Add GuiApp project

---
+++ CManager.Presentation.GuiApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CManager.Presentation.GuiApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
